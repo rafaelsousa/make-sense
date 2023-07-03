bb5f02cdc557db57fd2177e3f7f036a584947261
Rafael de Andrade Sousa <rafael.and@gmail.com>
2023-07-03 09:00:02 -0400

    Adjusting the services to the new file structure

--- server/main.py
+++ server/main.py
@@ -1,12 +1,13 @@
 import hashlib
 import logging
 import os
+import re
 import uuid
 from os.path import expanduser
 
 from flask import Flask, jsonify, send_file, request
 from flask_cors import CORS
-from ultralytics.yolo.utils import set_settings
+from ultralytics.yolo.utils import set_settings, get_settings
 from werkzeug.utils import secure_filename
 
 from aisieve.DetectView import detectionview
@@ -17,36 +18,43 @@ app = Flask(__name__)
 app.register_blueprint(trainview)
 app.register_blueprint(detectionview)
 
-currentDir = os.path.dirname(os.path.realpath(__file__))
-
 # This will enable CORS for the specific origin (http://localhost:5000)
 CORS(app)
 
 
 @app.route('/images', methods=['GET'])
 def get_images():
-    images = os.listdir(os.path.join(currentDir, 'images'))
+    imagesDir = os.path.join(get_settings()['datasets_dir'], 'images', 'peeble')
+    images = os.listdir(imagesDir)
+    # Filter by image extension
+    images = [image for image in images if re.search(r'\.(jpg|jpeg|png)$', image, re.IGNORECASE)]
     return jsonify(images=images)
 
 
 @app.route('/image/<image>', methods=['GET'])
 def get_image(image):
+    imagesDir = os.path.join(get_settings()['datasets_dir'], 'images', 'peeble')
     # Open the file and send it to the client
-    if os.path.exists(os.path.join(currentDir, 'images', image)):
-        return send_file(os.path.join(currentDir, 'images', image))
+    if os.path.exists(os.path.join(imagesDir, image)):
+        return send_file(os.path.join(imagesDir, image))
 
 
 @app.route('/annotations', methods=['GET'])
 def get_annotations():
-    annotations = os.listdir(os.path.join(currentDir, 'annotations'))
+    annotationsDir = os.path.join(get_settings()['datasets_dir'], 'images', 'peeble')
+    annotations = os.listdir(os.path.join(annotationsDir))
+    # This is called list comprehension, it's a shorthand for a for loop
+    annotations = [annotation for annotation in annotations if annotation.endswith('.txt')]
     return jsonify(annotations=annotations)
 
 
 @app.route('/annotation/<annotation>', methods=['GET'])
 def get_annotation(annotation):
+    annotationsDir = os.path.join(get_settings()['datasets_dir'], 'images', 'peeble')
+
     # Open the file and send it to the client
-    if os.path.exists(os.path.join(currentDir, 'annotations', annotation)):
-        return send_file(os.path.join(currentDir, 'annotations', annotation))
+    if os.path.exists(os.path.join(annotationsDir, annotation)):
+        return send_file(os.path.join(annotationsDir, annotation))
 
 
 @app.route('/annotation', methods=['POST'])
@@ -61,7 +69,8 @@ def publish_annotation():
 
     if file:
         filename = secure_filename(file.filename)
-        file.save(os.path.join(currentDir, 'annotations', filename))
+        annotationsDir = os.path.join(get_settings()['datasets_dir'], 'images', 'peeble')
+        file.save(os.path.join(annotationsDir, filename))
         logging.info(f"saved file {filename} to annotations folder")
         return 'File uploaded successfully', 200
 
