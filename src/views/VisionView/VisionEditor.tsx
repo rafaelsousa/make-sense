1023ffc329c60a8cc8d8068f49e14b2634ee9dda
Rafael de Andrade Sousa <rafael.and@gmail.com>
2023-07-06 10:11:02 -0400

    Adding the Runs Selection box

--- src/views/VisionView/VisionEditor.tsx
+++ src/views/VisionView/VisionEditor.tsx
@@ -1,7 +1,18 @@
-import React, {useEffect, useState} from 'react';
-import {Box, Button, LinearProgress, Stack, styled} from "@mui/material";
+import React, {useEffect} from 'react';
+import {
+    Box,
+    Button,
+    ImageList,
+    ImageListItem,
+    InputLabel,
+    LinearProgress,
+    MenuItem,
+    Select,
+    Stack,
+    styled
+} from "@mui/material";
 import {io} from "socket.io-client";
-import {EntryType} from "react-bbox-annotator";
+import styles from './Vision.module.css'
 
 interface TrainingData {
     currentEpoch: number;
@@ -15,20 +26,23 @@ const StyledBox = styled(Box)(({theme}) => ({
     display: 'flex',
     justifyContent: 'space-around',
     flexDirection: 'row',
+    padding: '10px',
+    gap: '20px',
 }));
 
 
 const VisionEditor: React.FC = () => {
 
     const [progress, setProgress] = React.useState(0);
-
-    const labels = ['peeble']
-
-    const [entries, setEntries] = useState<EntryType[]>([]);
+    const [currentEpoch, setCurrentEpoch] = React.useState(0);
+    const [totalEpochs, setTotalEpochs] = React.useState(0);
+    const [runs, setRuns] = React.useState([]);
+    const [selectedRun, setSelectedRun] = React.useState('')
+    const [imagesList, setImagesList] = React.useState([])
 
     const startTraining = () => {
         fetch('http://localhost:5000/train').then((response) => {
-            console.log(response)
+            console.log('training started')
         })
     }
 
@@ -45,15 +59,33 @@ const VisionEditor: React.FC = () => {
         })
 
         socket.on('training', (data) => {
-            console.log('from socket:', data.percentage)
+            setCurrentEpoch(data.currentEpoch)
+            setTotalEpochs(data.totalEpochs)
             setProgress(data.percentage)
         })
-
         return () => {
             socket.disconnect()
         }
     })
 
+    useEffect(() => {
+        fetch('http://localhost:5000/vision/runs').then((response) => {
+            response.json().then((data) => {
+                setSelectedRun(data.runs[0])
+                setRuns(data.runs)
+            })
+        })
+        fetch('http://localhost:5000/images').then((response) => {
+            response.json().then((data) => {
+                setImagesList(data.images)
+            })
+        })
+    }, [])
+
+
+    const handleRunSelection = (event) => {
+        setSelectedRun(event.target.value)
+    };
     return <>
         <StyledBox>
             <Button sx={{width: '150px'}} onClick={startTraining}>Train</Button>
@@ -67,10 +99,26 @@ const VisionEditor: React.FC = () => {
             }}>
                 <LinearProgress variant="determinate" value={progress} color={"secondary"}/>
             </Stack>
-            % of epochs completed
+            {currentEpoch} of {totalEpochs} epochs completed
+        </StyledBox>
+        <StyledBox style={{justifyContent: 'flex-start'}}>
+            <InputLabel id="runsLabel" className={styles.StyledBoxContent}>Runs</InputLabel>
+            <Select variant='outlined' className={styles.StyledBoxContent} color='secondary' label={"Runs"} labelId={"runsLabel"} value={selectedRun} onChange={handleRunSelection}>
+                {runs.map((run) => {
+                    return <MenuItem value={run}>
+                        {run}
+                    </MenuItem>
+                })}
+            </Select>
         </StyledBox>
         <StyledBox>
-            lalala
+            <ImageList sx={{ width: 200, height: '50vh' }} cols={1} rowHeight={164}>
+                {imagesList.map((item) => (
+                    <ImageListItem key={item.img}>
+                        <img src={`http://localhost:5000/image/${item}`} loading="lazy"/>
+                    </ImageListItem>
+                ))}
+            </ImageList>
         </StyledBox>
     </>
 }
